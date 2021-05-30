061bc615cb284e39afbe5c60b4fcabcd39e1fd41
lakshaymadhav <lakshaymadhav25@gmail.com>
2021-05-30 10:54:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reacttext
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/reacttext)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Peeps!</h1>
+      <h2>This is HariHaran.</h2>
+
+      <p>Currently Pursuing 3rd year of CSE @ SVCE</p>
+      <p>Roll: 180501050</p>
+    </div>
+  );
+}
